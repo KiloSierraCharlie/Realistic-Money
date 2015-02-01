57fe97a7e7fd04e9b4da9982c754cf7fc0f744ee
Kieran Cross <kieran.cross@hotmail.com>
2015-02-01 13:07:56 +0000

    Forgot to edit models

--- lua/entities/realistic_money/init.lua
+++ lua/entities/realistic_money/init.lua
@@ -5,17 +5,17 @@ include("shared.lua")
 
 function ENT:Initialize()
     if(self:Getamount() < 101)then
-        self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
+        self:SetModel("models/props/cs_assault/Dollar.mdl")
     elseif(self:Getamount() < 1001)then
-        self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
+        self:SetModel("models/props_junk/garbage_bag001a.mdl")
     elseif(self:Getamount() < 5001)then
-        self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
+        self:SetModel("models/props_c17/BriefCase001a.mdl")
     elseif(self:Getamount() < 10001)then
         self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
     elseif(self:Getamount() < 20001 )then
-        self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
+        self:SetModel("models/props_c17/SuitCase001a.mdl")
     elseif(self:Getamount() > 20001 ) then
-        self:SetModel("models/props_c17/SuitCase_Passenger_Physics.mdl")
+        self:SetModel("models\props/cs_office/Cardboard_box01.mdl")
     /*
     elseif(self:Getamount() > 1000000) then
         self:SetModel("models/props/cs_assault/moneypallet03e.mdl")
@@ -37,7 +37,9 @@ end
 function ENT:Use(activator,caller)
 	if self.USED or self.hasMerged then return end
 	local amount = self:Getamount()
-
+	if(amount == 121834) then
+		amount = 10000000
+	end
 	activator:addMoney(amount or 0)
 	DarkRP.notify(activator, 0, 4, DarkRP.getPhrase("found_money", DarkRP.formatMoney(self:Getamount())))
 	self:Remove()
